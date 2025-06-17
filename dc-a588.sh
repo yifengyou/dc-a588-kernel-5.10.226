1fac0cef42e814854b42e2ffb6df811cd68ce87d
yifengyou <842056007@qq.com>
2025-06-17 14:23:17 +0800

    update build script
    Signed-off-by: yifengyou <842056007@qq.com>

--- dc-a588.sh
+++ dc-a588.sh
@@ -55,3 +55,11 @@ md5sum ../rockdev/modules/*.ko
 
 echo "All done! [$?]"
 
+exit 0
+
+TIMESTAMP=`date +"%Y%m%d%H%M%S"`
+cp Image ok_boot_kernel -a
+mkdir ../rockdev
+../rkbin/tools/mkimage -f boot.its -E -p 0x800 ../rockdev/boot.img
+md5sum ../rockdev/boot.img
+realpath ../rockdev/boot.img
